ca8521b3d3390860fa33da09e4f7825976d557d2
Michelle Janowiecki <mjanowi3@jh.edu>
2020-02-24 15:39:11 -0500

    Update matchInfoInTwoSpreadsheetsByIdentifier.py

--- matchInfoInTwoSpreadsheetsByIdentifier.py
+++ matchInfoInTwoSpreadsheetsByIdentifier.py
@@ -17,9 +17,12 @@ if args.file2:
 else:
     filename2 = input('Enter filename (including \'.csv\'): ')
 
+new_file = filename.replace('.csv', '')
+new_file2 = filename2.replace('.csv', '')
 dt = datetime.now().strftime('%Y-%m-%d %H.%M.%S')
+name_string = new_file+'_'+new_file2+'matchedById_'
 
-f = csv.writer(open('matchedById_'+dt+'.csv', 'w', encoding='utf-8'))
+f = csv.writer(open(name_string+dt+'.csv', 'w', encoding='utf-8'))
 f.writerow(['dc.identifier']+['full.identifier']+['uri']+['link'])
 
 
