f8a11f57702f8152ffd0fd66748138d01d86bf8d
Michelle Janowiecki <michelle@MSEL-CATMAC01.local>
2020-12-02 09:36:00 -0500

    added columnName variable

--- merge/mergeLeftTwoSheets.py
+++ merge/mergeLeftTwoSheets.py
@@ -5,6 +5,7 @@ from datetime import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file')
 parser.add_argument('-f2', '--file2')
+parser.add_argument('-c', '--columnName')
 args = parser.parse_args()
 
 if args.file:
@@ -15,6 +16,11 @@ if args.file2:
     filename2 = args.file2
 else:
     filename2 = input('Enter second filename (including \'.csv\'): ')
+if args.columnName:
+    columnName = args.columnName
+else:
+    columnName = input('Enter column to merge on: ')
+
 
 df_1 = pd.read_csv(filename, header=0)
 print(df_1.columns)
@@ -22,7 +28,7 @@ df_2 = pd.read_csv(filename2, header=0)
 print(df_2.columns)
 
 
-frame = pd.merge(df_1, df_2, how='left', on=['itemID'], suffixes=('_1', '_2'))
+frame = pd.merge(df_1, df_2, how='left', on=[columnName], suffixes=('_1', '_2'))
 
 frame = frame.drop_duplicates()
 
