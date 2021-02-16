00af7ddcaa21f65ac17711da4600b86b47d3dc23
Michelle Janowiecki <michelle@MSEL-CATMAC01.local>
2021-02-16 14:26:51 -0500

    add scripts

---
+++ evaluate/findDuplicatedIdentifierPairs.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file')
+args = parser.parse_args()
+
+if args.file:
+    filename = args.file
+else:
+    filename = input('Enter filename (including \'.csv\'): ')
+
+
+df = pd.read_csv(filename)
+
+duplicates = df.duplicated(subset=['handle', 'bitstreams'], keep=False)
+
+duplicate_list = []
+for index, value in duplicates.iteritems():
+    if value is True:
+        duplicate_list.append(index)
+
+for duplicate in duplicate_list:
+    print(df.iloc[duplicate])
